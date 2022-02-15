ddf96cac3eeeb5ffbb574d370e370b8dfc112fa1
Maurice Johnson <edmundhaley@gmail.com>
2022-02-15 11:58:54 -0500

    Create ResponseBody.java

---
+++ src/main/java/com/wmf/model/ResponseBody.java
@@ -0,0 +1,40 @@
+package com.wmf.model;
+
+/**
+ * Class: ResponseBody:  
+ * Scope: Model Object for API Response
+ * ------------------------------------------------------------
+ * Date         Resource            Description
+ * ------------------------------------------------------------
+ * 02/15/2022   Maurice Johnson     INIT...         
+ * ------------------------------------------------------------
+ */
+public class ResponseBody {
+	private String content;
+
+    public ResponseBody() {
+    	this.content = null; 
+    }
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	@Override
+	public String toString() {
+		//log.info("Within the ResponseBody...\n");
+		StringBuilder modelObject = new StringBuilder();
+		
+		modelObject.append("{\n");
+		modelObject.append("ResponseBody: {\n");
+		modelObject.append("content: ").append(content).append("\n");	
+		modelObject.append("}").append("\n");
+		modelObject.append("}");
+		
+		return modelObject.toString();
+	}
+}
